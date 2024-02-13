9747cc90167c02a36c61ec343f7e3ec24ab8085e
zeppoo <reinverhaag@gmail.com>
2024-02-13 15:33:21 +0100

    GUI update

--- Spitfire_Flight_Sim/Assets/Scripts/AeroSurface.cs
+++ Spitfire_Flight_Sim/Assets/Scripts/AeroSurface.cs
@@ -8,4 +8,6 @@ public class AeroSurface : ScriptableObject
     [SerializeField] private Vector2 surfaceSize;
     [SerializeField] private float flapsSize;
     [SerializeField] private AnimationCurve liftSlope;
+    public Vector3 normal = Vector3.up;
+    public float size = 1f;
 }
---
+++ Spitfire_Flight_Sim/Assets/Scripts/AeroSurfaceAttach.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AeroSurfaceAttach : MonoBehaviour
+{
+    [SerializeField] private AeroSurface _aeroSurface;
+}
